0e2bf08ffbb098dd2cf8e926a65e543a2cc48fac
Nikolay Manolov <nikolay.manolov@asteasolutions.com>
2023-02-14 17:16:28 +0200

    Relay get_cache and ltc_volume #2, #4

--- src/routes/token.route.ts
+++ src/routes/token.route.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { getTokenInfo, listTokens, getTokenCurrencyTotal } from "../services/token.service";
+import { getTokenInfo, listTokens, getTokenCurrencyTotal, getCache, getLtcVolume } from "../services/token.service";
 import { get } from "./helper";
 
 export const tokenRoute = (fastify: FastifyInstance, opts: any, done: any) => {
@@ -17,6 +17,29 @@ export const tokenRoute = (fastify: FastifyInstance, opts: any, done: any) => {
         const _propid = parseInt(propertyId);
         return getTokenCurrencyTotal(_propid);
     });
+
+    get(fastify, '/:propertyId/cache', (request) => {
+        const { propertyId } = request.params as { propertyId: string };
+        const { cacheType } = request.query as { cacheType: string };
+        const _propId = parseInt(propertyId);
+        return getCache(_propId, cacheType);
+    });
+
+    get(fastify, '/:propertyId/ltc_volume', (request) => {
+        const { propertyId } = request.params as { propertyId: string };
+        const {
+            startBlock,
+            endBlock
+        } = request.query as {
+            startBlock: string,
+            endBlock: string,
+        };
+        const _propId = parseInt(propertyId);
+        const _startBlock = startBlock && parseInt(startBlock);
+        const _endBlock = endBlock && parseInt(endBlock);
+
+        return getLtcVolume(_propId, _startBlock, _endBlock);
+    })
 
     done();
 }
--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,7 +1,5 @@
 import { rpcClient } from "../config/rpc.config"
 
-
-
 export const listTokens = () => {
     return rpcClient.call('tl_listproperties');
 }
@@ -12,4 +10,31 @@ export const getTokenInfo = (propid: number) => {
 
 export const getTokenCurrencyTotal = (propid: number) => {
     return rpcClient.call('tl_getcurrencytotal', propid);
+}
+
+enum CacheType {
+    NativeFees = 0,
+    OracleFees = 1,
+    SpotFees = 2,
+    Total
+};
+
+const parseCacheType = (p: string): CacheType => {
+    const i = Number(p);
+    if (i < 0) {
+        throw { error: 'Invalid Cache Type' };
+    }
+    if (i >= 0 && i <= 2) {
+        return i as CacheType;
+    }
+    return CacheType.Total;
+}
+
+export const getCache = (propId: number, type: string) => {
+  const cacheType = parseCacheType(type);
+  return rpcClient.call('tl_getcache', propId, cacheType);
+}
+
+export const getLtcVolume = (propId: number, firstBlock: number, secondBlock: number) => {
+    return rpcClient.call('tl_get_ltcvolume', propId, firstBlock, secondBlock);
 }
