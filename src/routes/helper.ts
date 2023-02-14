64e42a018da48261d6bbfe046e360e8b2a800afc
Nikolay Manolov <nikolay.manolov@asteasolutions.com>
2023-02-14 17:00:32 +0200

    Add type information to helper - Improves usability

--- src/routes/helper.ts
+++ src/routes/helper.ts
@@ -1,4 +1,11 @@
-export const get = (fastify, path, handler) => {
+import { FastifyInstance, FastifyRequest, RouteHandlerMethod } from "fastify";
+import { RouteGenericInterface } from "fastify/types/route";
+import { IncomingMessage, Server } from "http";
+
+
+type HandlerType = (request: FastifyRequest<RouteGenericInterface, Server, IncomingMessage, unknown>) => Promise<any>;
+
+export const get = (fastify: FastifyInstance, path: string, handler: HandlerType) => {
   fastify.get(path, (request, reply) => {
       return handler(request)
           .then(res => reply.send(res))
